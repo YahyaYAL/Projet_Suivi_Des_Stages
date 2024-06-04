0b1abbc4577b774995a73d8f347918b8a6e3a1bd
YahyaYAL <127733898+YahyaYAL@users.noreply.github.com>
2024-06-04 10:56:05 +0200

    Delete Projet_Suivi_Des_Stages/Projet_Suivi_Stages directory

--- Projet_Suivi_Des_Stages/Projet_Suivi_Stages/php/connexion_mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_username = 'root';
-$db_password = 'Kirikou202209!';
-$db_name = 'Suivi_Des_Stages';
-$db_host = 'localhost';
-$db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-or die('could not connect to database');
-    
-?>
